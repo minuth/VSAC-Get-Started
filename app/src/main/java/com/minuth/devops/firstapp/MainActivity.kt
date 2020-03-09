7aa8f8241d57b1b34e865ba2e4405382579cd351
minuth <minuthprom321@gmail.com>
2020-03-09 22:18:09 +0700

    init

---
+++ app/src/main/java/com/minuth/devops/firstapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.minuth.devops.firstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
